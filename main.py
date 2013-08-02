8d6b066f29fcba4198cd8642c7b12c78c924df8c
ekianjo <ekianjo@gmail.com>
2013-08-02 11:25:06 +0900

    Update main.py
    drafted first key commands.

--- main.py
+++ main.py
@@ -2,3 +2,25 @@
 #use openweather API
 #interface should be super clean and professional looking.
 #controls, simple and keyboard based. 
+
+import json
+
+def getdata(city):
+  unit="metric"
+  lang="en"
+  forecast=3
+  datanow=urllib('http://api.openweathermap.org/data/2.5/weather?q={0}&units={1}&lang={2}'.format(city,unit,lang))
+  dataforecast=urllib('http://api.openweathermap.org/data/2.5/forecast/daily?q={0}&units={1}&lang={2}&cnt={3}'.format(city,unit,lang,forecast))
+  
+def splitjsondata(jsondata):
+  pass
+  
+def geticon(weathercode):  
+  icon=urllib('http://openweathermap.org/img/w/{0}'.format(weathercode))
+  
+def savepref():
+  #save recent cities
+  #save favorites
+  
+
+#display window (cairo) with the appropriate information and graphics
